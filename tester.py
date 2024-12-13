7e1c9af06fc9491dee169677d1f75645dcdd571d
ThienMP <99773255+ThienMP@users.noreply.github.com>
2024-12-13 16:08:57 -0500

    Update tester.py

--- tester.py
+++ tester.py
@@ -13,14 +13,13 @@ serverStarter = threading.Thread(target=server)
 
 c1 = threading.Thread(target=client_Side, args=("Client 1",))
 c2 = threading.Thread(target=client_Side, args=("Client 2",))
-c3 = threading.Thread(target=client_Side, args=("Client 3",))
+
 
 serverStarter.start()
 c1.start()
 c2.start()
-c3.start()
+
 
 serverStarter.join()
 c1.join()
 c2.join()
-c3.join()
